1e48858afb91ef1833d5b6862f64195498d7c37b
wk645 <wk645@nyu.edu>
2017-06-11 23:47:04 -0400

    loop lab

--- looping.rb
+++ looping.rb
@@ -1,5 +1,11 @@
-def looping
-	#your code here
+def looping(phrase = "Wingardium Leviosa")
+    puts "#{phrase}"
 end
 
 #call your method here
+
+counter = 0
+loop do
+counter += 1
+puts "Wingardium Leviosa"
+end
